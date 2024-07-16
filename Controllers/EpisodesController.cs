5f180c44cf150713d2aeaeb246c2d8d1592b7c61
ThaiBaoNguyen <nthaibao1712@gmail.com>
2024-07-17 00:01:09 +0700

    Start EpisodeController

--- Controllers/EpisodesController.cs
+++ Controllers/EpisodesController.cs
@@ -1,12 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using API_Movies.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace API_Movies.Controllers
 {
     public class EpisodesController : Controller
     {
-        public IActionResult Index()
+        private readonly MoviesDbContext _moviesDbContext;
+
+        public EpisodesController(MoviesDbContext moviesDbContext)
         {
-            return View();
+            _moviesDbContext = moviesDbContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<Episode>> Get()
+        {
+            var episodes = await _moviesDbContext.Episodes.ToList();
+            return episodes;
         }
     }
 }
